638a0964480283ee34608084a545a7e56b151983
Jack <105589257+Qautno166@users.noreply.github.com>
2023-05-16 13:59:47 +0000

    Add files via upload

---
+++ src/component/Main.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Main() {
+  return (
+    <main>
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first relased in 2013 </li>
+        <li> Was originaly created by Jordan Wlke</li>
+        <li>Has well over 100k stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousand of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
+  );
+}
+export default Main;
---
+++ src/component/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+function Navbar() {
+  return (
+    <nav>
+      <img className="pic" src="logo192.png" alt="" />
+      <h2 className="navLeft"> ReactFacts</h2>
+      <h3 className="navRight">React Course - Project 1</h3>
+    </nav>
+  );
+}
+export default Navbar;
